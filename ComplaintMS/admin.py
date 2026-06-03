3686da559b98a56edc9533ee58177009a8c0540a
Riya Hirani <riyahirani111@gmail.com>
2026-06-03 18:55:22 +0530

    Initial commit

---
+++ ComplaintMS/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Profile,Complaint,Grievance
+
+class CAdmin(admin.ModelAdmin):
+    list_display = ('user','Subject','Type_of_complaint','Description','Time','status')
+    list_editable = ('status',)
+
+
+admin.site.register(Profile)
+admin.site.register(Complaint,CAdmin)
+admin.site.register(Grievance)
